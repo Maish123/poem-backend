0011c6a27ad36cfeb5122212de59c19b39963480
Denis Maina <Denis.maina@student.moringaschool.com>
2022-09-09 09:52:39 +0300

    updated seeded data to the db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,38 @@
 puts "🌱 Seeding spices..."
 
 # Seed your database here
+## Author details
+10.times do
+    Author.create(name: Faker::Name.name)
+end
+
+3.times do
+    Genre.create(genre_name: Faker::Music.genre)
+end
+
+30.times do
+    Poem.create(
+        title: Faker::Lorem.word,
+        content: Faker::Quote.matz,
+        likes:rand(1...50),
+        author_id:rand(1...10),
+        genre_id:rand(1..3)
+    )
+end
+
+## Genre details
+# Genre.create(name: "confessional")
+# Genre.create(name: "Sonet")
+# Genre.create(name: "Sonet")
+
+## poem details
+# Poem.create(
+#     title:
+#     content:
+#     likes: rand(1...10)
+#     author_id:rand(1...10)
+#     genre_id: rand(1...3)
+# )
+
 
 puts "✅ Done seeding!"
